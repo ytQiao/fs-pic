28ec2d0b8876b5d4d546917f2a8d5a9590d48212
guows <guowenshuai8207@163.com>
2020-10-14 10:22:59 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,260 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <!-- 钱包选择器 -->
+    <a-select
+      placeholder="选择钱包查询"
+      style="width: 400px"
+      :show-arrow="false"
+      :filter-option="false"
+      :not-found-content="null"
+      @change="handleAddressChange">
+      <a-select-option v-for="item in walletAddress" :key="item.address">
+        {{item.name}}
+      </a-select-option>
+    </a-select>
+
+  <!-- 筛选 -->
+    <div style="margin: 10px">
+
+    <a-label>选择类型： </a-label>
+    <a-select
+      v-model="search.filtertype"
+      placeholder="选择类型"
+      style="width: 200px"
+      :filter-option="false"
+      :not-found-content="null">
+      <a-select-option v-for="item in filters.typefilter" :key="item.value">
+        {{item.text}}
+      </a-select-option>
+    </a-select>
+
+    <a-label style="margin-left: 100px">额度条件： </a-label>
+    <a-select
+      v-model="search.filteroperator"
+      placeholder="条件"
+      style="width: 100px"
+      :filter-option="false"
+      :not-found-content="null">
+      <a-select-option v-for="item in filters.operatorfilters" :key="item.value">
+        {{item.text}}
+      </a-select-option>
+    </a-select>  
+
+    <a-input-number style="width: 100px" v-model="search.filtervalue" placeholder="200"/>
+
+    <a-select
+      v-model="search.filterunit"
+      placeholder="单位"
+      style="width: 100px"
+      :filter-option="false"
+      :not-found-content="null">
+      <a-select-option v-for="item in filters.unitfilters" :key="item.value">
+        {{item.text}}
+      </a-select-option>
+    </a-select>       
+
+    <a-button type="primary" @click="submitFilter">
+        查询
+    </a-button>
+    </div>
+  <!-- 表格 -->
+    <a-table :columns="columns" :data-source="tableData"
+        :pagination="pagination"
+        @change="handleTableChange"
+      >
+      <span slot="timestamp" slot-scope="timestamp">{{ formatterTime(timestamp) }}</span>
+      <span slot="message" slot-scope="message">
+        <span v-if="message">{{ message }}</span>
+        <span v-else>N/A</span>
+      </span>
+      <span slot="value" slot-scope="value">{{ formatFilValue(value) }}</span>
+
+    </a-table>
+
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import moment from 'moment'
+
+const columns = [
+  {
+    title: '区块高度',
+    dataIndex: 'height',
+    key: 'height',
+
+  },
+  {
+    title: '时间',
+    dataIndex: 'timestamp',
+    key: 'timestamp',
+    scopedSlots: { customRender: 'timestamp'},
+  },
+  {
+    title: '消息id',
+    dataIndex: 'message',
+    key: 'message',
+    ellipsis: true,
+    scopedSlots: { customRender: 'message'},
+  },
+  {
+    title: 'from',
+    dataIndex: 'from',
+    key: 'from',
+    ellipsis: true,
+  },
+  {
+    title: 'to',
+    dataIndex: 'to',
+    key: 'to',
+    ellipsis: true,
+  },
+  {
+    title: '值',
+    key: 'value',
+    dataIndex: 'value',
+    scopedSlots: { customRender: 'value'},
+
+  },
+  {
+    title: '类型',
+    dataIndex: 'type',
+    key: 'type',
+  },
+];
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  mounted() {
+    this.getWallet()
+  },
+
+  methods: {
+    handleAddressChange(value) {
+       this.value = value
+       this.getTransfers(1, 20)
+    },
+
+    getWallet: function() {
+      axios.get(this.apiPrefix + '/wallet').then((response) => {
+        this.walletAddress = response.data
+      })
+    },
+    getTransfers: function(page, pagesize) {
+      var conditions = this.buildCondition()
+      var u = this.apiPrefix + '/wallet/' + this.value + "?page=" + page + "&pageSize=" + pagesize
+      if (conditions.length > 0) {
+        u += conditions
+      }
+      axios.get(u).then((response) => {
+        this.tableData = response.data.data
+        this.pagination.total = response.data.total
+      })
+    },
+
+    handleTableChange: function(pagination, filters, sorter, { currentDataSource }) {
+      this.getTransfers(pagination.current, pagination.pageSize)
+      console.log(filters)
+      console.log(sorter)
+      console.log(currentDataSource)
+    },
+
+    formatterTime: function(val) {
+      return val ? moment(val*1000).format('YYYY-MM-DD HH:mm:ss') : ''
+    },
+
+    formatFilValue: function(val) {
+      var fvalue = +(val)
+      
+      if (Math.abs(fvalue) > Math.pow(10, 15) ) {
+        return (fvalue/Math.pow(10,18)).toFixed(5) + " FIL"
+      }
+      if (Math.abs(fvalue) > Math.pow(10, 6) ) {
+        return (fvalue/Math.pow(10,9)).toFixed(5) + " nonaFIL"
+      }
+      return fvalue.toFixed(5) + " attoFIL"
+    },
+
+    buildCondition: function() {
+      var qstr = ""
+      if (this.search.filtertype.length > 0) {
+        qstr += "&type="+this.search.filtertype
+      }
+      if (this.search.filteroperator.length >0) {
+        qstr += "&op=" + this.search.filteroperator + "&value=" + this.search.filtervalue*this.search.filterunit
+      }
+      return qstr
+    },
+    submitFilter: function() {
+      this.getTransfers(1, 20)
+    }
+  },
+  data: function() {
+    return {
+      walletAddress:  [],
+      value: "",
+      apiPrefix: "/v1",
+      // apiPrefix: "http://127.0.0.1:8080/v1",
+      columns: columns,
+      tableData: [],
+      pagination: {
+        total: 20,
+        pageSize: 20,
+        defaultCurrent: 1,
+      },
+      search: {
+        filtervalue: 0,
+        filtertype: "",
+        filterunit: 1,
+        filteroperator: "",
+      },
+      filters: {
+        typefilter: [
+          {
+            text: 'NONE',
+            value: '',
+          },
+          {
+            text: 'receive',
+            value: 'receive',
+          }, {
+            text: 'send',
+            value: 'send',
+          }, {
+            text: 'burn-fee',
+            value: 'burn-fee',
+          }, {
+            text: 'miner-fee',
+            value: 'miner-fee',
+          }, {
+            text: 'reward',
+            value: 'reward',
+          }, {
+            text: 'burn',
+            value: 'burn',
+          }, 
+        ],
+        unitfilters: [
+          {text: 'FIL', value: Math.pow(10,18)},
+          {text: 'nanoFIL', value: Math.pow(10,9)},
+          {text: 'attoFIL', value: 1},
+        ],
+        operatorfilters: [
+          {text: 'NONE', value: ''},
+          {text: '=', value: '$eq'},
+          {text: '>', value: '$gt'},
+          {text: '<', value: '$lt'},
+        ],
+
+      },
+
+    }
+  },
+
 }
 </script>
 
